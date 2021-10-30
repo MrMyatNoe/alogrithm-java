6275c0caa9997b74b9ff81b9440045c11a6b3aa0
MrMyatNoe <thetmyatnoe223@gmail.com>
2021-10-30 15:13:18 +0630

    feat: pow

---
+++ src/com/algo/integer/Pow.java
@@ -0,0 +1,12 @@
+package com.algo.integer;
+
+public class Pow {
+
+    public static void main(String[] args) {
+        pow(1,0.25);
+    }
+    
+    public static void pow(double n1,double n2) {
+        System.out.println(Math.pow(n1,n2));
+    }
+}
