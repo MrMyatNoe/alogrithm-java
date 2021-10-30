24b3799f2d67e922d1d1aa257c2c7f256ef1f32e
MrMyatNoe <thetmyatnoe223@gmail.com>
2021-10-30 14:49:25 +0630

    feat: palindrome

---
+++ src/com/algo/integer/Palindrome.java
@@ -0,0 +1,19 @@
+package com.algo.integer;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        int r, sum = 0, temp;
+        int n = 2002;
+        temp = n;
+        while (n > 0) {
+            r = n % 10; // getting remainder // 4 5
+            sum = (sum * 10) + r;
+            n = n / 10;
+        }
+        if (temp == sum)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+    }
+}
