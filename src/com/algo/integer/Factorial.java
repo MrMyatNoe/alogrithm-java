4fa63aeac2cab21fd606ce171cf8657f3ad4a8d1
MrMyatNoe <thetmyatnoe223@gmail.com>
2022-03-07 00:03:52 +0630

    chore(factorial): solved another way

--- src/com/algo/integer/Factorial.java
+++ src/com/algo/integer/Factorial.java
@@ -3,14 +3,22 @@ package com.algo.integer;
 public class Factorial {
 
     public static void main(String[] args) {
-        int fact = factorial(4);
+        int fact = factorial(30);
         System.out.println("factorial is : " + fact);
     }
 
     static int factorial(int n) {
-        if (n == 0)
-            return 1;
-        else
-            return (n * factorial(n - 1));
+        int fact = 1;
+        for (int i = 1; i < n; i++) {
+            fact = fact * i;
+        }
+     return fact;
+//        if (n == 0)
+//            return 1;
+//        else
+//            return (n * factorial(n - 1));
+        
+        
+        
     }
 }
